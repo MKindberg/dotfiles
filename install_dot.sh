c90d504dacf195ff079edfe74a24a189b8c15ce5
Marcus <marcus.kindberg@outlook.com>
2018-01-06 22:58:29 +0100

    improved installation

--- install_dot.sh
+++ install_dot.sh
@@ -2,27 +2,40 @@
 DOT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 function backup {
-  local backup_folder="$HOME/dotfiles_backup"
+  backup_folder="$HOME/dotfiles_backup"
   mkdir -p $backup_folder
-  mv ~/.bashrc ${backup_folder}/
-  mv ~/.vimrc ${backup_folder}/
+  if [ $1 == "bash" ] || [ $1 == "all" ]; then
+    cp ~/.bashrc ${backup_folder}/
+  fi
+  if [ $1 == "vim" ] || [ $1 == "all" ]; then
+    cp ~/.vimrc ${backup_folder}/
+  fi
 }
 
-function install_clean {
-  echo "source ${DOT_DIR}/bash/bashrc" > ~/.bashrc
-  echo "source ${DOT_DIR}/vim/vimrc" > ~/.vimrc
-}
+function install {
+  if [ -n $2 -a $2 == "clean" ]; then
+    if [ $1 == "bash" ] || [ $1 == "all" ]; then
+      echo "" > ~/.bashrc
+    fi
+    if [ $1 == "vim" ] || [ $1 == "all" ]; then
+      echo "" > ~/.vimrc
+    fi
+  fi
 
-function install_append {
-  echo -e "source ${DOT_DIR}/bash/bashrc\n$(cat ~/.bashrc)" > ~/.bashrc
-  echo -e "source ${DOT_DIR}/vim/vimrc\n$(cat ~/.vimrc)" > ~/.vimrc
+  if [ $1 == "bash" ] || [ $1 == "all" ]; then
+    echo -e "source ${DOT_DIR}/bash/bashrc\n$(cat $HOME/.bashrc)" > ~/.bashrc
+    installed=1
+  fi
+  if [ $1 == "vim" ] || [ $1 == "all" ]; then
+    echo -e "source ${DOT_DIR}/vim/vimrc\n$(cat $HOME/.vimrc)" > ~/.vimrc
+    installed=1
+  fi
+  if [ -z $installed ]; then
+    echo "Invalid first argument, must be \'bash\', \'vim\' or \'all\'"
+  fi
 }
 
 backup
 
-if [ -n $1 ] -a [ $1 == "append" ]; then
-  install_append
-else
-  install_clean
-fi
+install $1 $2
 
