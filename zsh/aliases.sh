f192f95c4f7180a88a8e7cc63873b5c741d631f6
Marcus <marcus.kindberg@outlook.com>
2018-03-07 21:13:49 +0100

    Added aliases for navigating directories

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -35,3 +35,22 @@ alias -s c=vim
 alias -s h=vim
 alias -s cpp=vim
 alias -s html=firefox
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+alias -- -='cd -'
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
