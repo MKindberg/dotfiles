198fa6bbf7831945e7dbc36e679d60e0c58c4f4d
Marcus <marcus.kindberg@outlook.com>
2018-08-10 21:10:41 +0200

    Added manpage colors

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -55,3 +55,14 @@ ls_mi="mi=9;37:" # Missing symlinks are strikethrough and grey
 ls_or="or=9;90:" # Orphaned symlinks are strikethrough and dark grey
 
 export LS_COLORS="$ls_dir$ls_fi$ls_ex$ls_ln$ls_mi$ls_or"
+
+#colors for man pages
+man() {
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    command man "$@"
+}
