66ffd16ec0a4a4501239de78daae85484e68b090
Marcus <marcus.kindberg@protonmail.com>
2020-11-28 11:56:21 +0100

    zsh: clean up plugin file

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,19 +1,13 @@
 if [[ $ZSH_VERSION -ge 5.3 ]]; then
-
-  zinit ice pick"zsh-syntax-highlighting.zsh" wait'!0'
-  zinit light zsh-users/zsh-syntax-highlighting
-
-  zinit ice pick"zsh-autosuggestions.zsh" wait'!0'
-  zinit light zsh-users/zsh-autosuggestions
-
-  zinit ice pick"zsh-autocomplete.plugin.zsh" wait'!0'
+  zinit ice pick"zsh-autocomplete.plugin.zsh"
   zinit light marlonrichert/zsh-autocomplete
 
-else
+  TURBO=wait'!0'
 
-  zinit ice pick"zsh-syntax-highlighting.zsh"
+fi
+
+  zinit ice pick"zsh-syntax-highlighting.zsh" $TURBO
   zinit light zsh-users/zsh-syntax-highlighting
 
-  zinit ice pick"zsh-autosuggestions.zsh"
+  zinit ice pick"zsh-autosuggestions.zsh" $TURBO
   zinit light zsh-users/zsh-autosuggestions
-fi
