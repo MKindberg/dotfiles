6b3f646894b8f56b8b64d7bb741c1e5ca50ea50e
Marcus <marcus.kindberg@protonmail.com>
2020-11-28 22:50:51 +0100

    zsh: Change autocomplete behavior and add fzf-tab outcommented

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -2,9 +2,13 @@ if [[ $ZSH_VERSION -ge 5.3 ]]; then
   zinit ice pick"zsh-autocomplete.plugin.zsh"
   zinit light marlonrichert/zsh-autocomplete
 
+#  zinit ice pick"fzf-tab.plugin.zsh"
+#  zinit light Aloxaf/fzf-tab
+
   TURBO=wait'!0'
 
 fi
+
 
   zinit ice pick"zsh-syntax-highlighting.zsh" $TURBO
   zinit light zsh-users/zsh-syntax-highlighting
