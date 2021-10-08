327ae4e1aabfe85bb7a29dfed37fd5f54fd2ab13
Marcus <marcus.kindberg@protonmail.com>
2021-10-08 20:30:29 +0200

    zsh: Add posibility to run command in tmux split

--- zsh/keybind.sh
+++ zsh/keybind.sh
@@ -2,3 +2,13 @@ bindkey "^[" vi-cmd-mode
 bindkey '^[[Z' reverse-menu-complete
 bindkey "^[%" vi-match-bracket
 bindkey -s "^Z" "%\n"
+
+stty -ixon # Unbind current ctrl+s behavior
+launch-in-split() { Launch command in new tmux split
+  if [[ -n "$TMUX" ]]; then
+    BUFFER="tmux split-window -h \"tmux select-pane -l; echo -e \\\"$BUFFER\n-----\n\\\"; $BUFFER; read\""
+    zle accept-line
+  fi
+}
+zle -N launch-in-split
+bindkey "^s" launch-in-split
