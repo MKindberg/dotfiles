6445cadb2540db6fcca5c0608653aefbf3dd7a53
Marcus <marcus.kindberg@protonmail.com>
2022-03-20 22:00:03 +0100

    vim: Remove sidebar setup
    It shouldn't have been added yet

--- lua/vimrc.lua
+++ lua/vimrc.lua
@@ -510,23 +510,6 @@ require('lualine').setup {
 }
 -- }}}
 
-require("sidebar-nvim").setup({
-    disable_default_keybindings = 0,
-    bindings = nil,
-    open = false,
-    side = "left",
-    initial_width = 35,
-    hide_statusline = false,
-    update_interval = 1000,
-    sections = { "datetime", "git", "diagnostics", "symbols" },
-    section_separator = {"", "-----", ""},
-    containers = {
-        attach_shell = "/bin/sh", show_all = true, interval = 5000,
-    },
-    datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
-    todos = { ignored_paths = { "~" } },
-})
-
 require("neogit").setup{
   disable_commit_confirmation = true
 }
