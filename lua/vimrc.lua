ee939728f959959e6ad43df4dcd1f10ad1642040
Marcus <marcus.kindberg@protonmail.com>
2022-06-18 18:44:53 +0200

    vim: Make lua lsp recognize nvim functions

--- lua/vimrc.lua
+++ lua/vimrc.lua
@@ -307,7 +307,28 @@ if(os.execute("bash -c 'command -v bash-language-server'") == 0) then
   require'lspconfig'.bashls.setup{}
 end
 if(os.execute("bash -c 'command -v lua-language-server'") == 0) then
-  require'lspconfig'.sumneko_lua.setup{}
+  require'lspconfig'.sumneko_lua.setup{
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  }
 end
 
 keymap('n', '<Leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', {expr = false, noremap = true})
