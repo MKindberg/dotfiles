eab3f471171321b0251f67e7dde11a65ae2f72e9
Marcus <6180175+MKindberg@users.noreply.github.com>
2022-09-10 15:05:39 +0200

    vim: Fix issue with lua lsp

--- lua/vimrc.lua
+++ lua/vimrc.lua
@@ -268,6 +268,7 @@ if(os.execute("bash -c 'command -v lua-language-server'") == 0) then
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
